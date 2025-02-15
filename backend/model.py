3a26ef69e57c02bce4d26bc6e606ac4f480b8dc6
Tvesha Shah <tveshashah13@gmail.com>
2025-02-14 23:06:59 -0500

    set up classifer playground

--- backend/model.py
+++ backend/model.py
@@ -1,8 +1,19 @@
 import os
 from dotenv import load_dotenv
+import argparse
 
 # Load the .env file
 load_dotenv()
 
 # Access the API key
 api_key = os.getenv('API_KEY')
+
+parser = argparse.ArgumentParser(description="Classifer")
+parser.add_argument("paragraph", type=str, help="Input paragraph")
+args = parser.parse_args()
+print(args.paragraph)
+
+
+classified_model = "angry"
+
+print(classified_model)
