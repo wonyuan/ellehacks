9fe03c36ab26050cb76e81ba411c582883420eea
Tvesha Shah <tveshashah13@gmail.com>
2025-02-15 00:42:06 -0500

    classify

--- backend/model.py
+++ backend/model.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import argparse
+import cohere
 
 # Load the .env file
 load_dotenv()
@@ -8,12 +9,17 @@ load_dotenv()
 # Access the API key
 api_key = os.getenv('API_KEY')
 
+if api_key is None:
+    raise ValueError("API key not found. Please ensure the .env file contains the correct 'API_KEY'.")
+
 parser = argparse.ArgumentParser(description="Classifer")
 parser.add_argument("paragraph", type=str, help="Input paragraph")
 args = parser.parse_args()
 print(args.paragraph)
 
 
-classified_model = "angry"
-
-print(classified_model)
+co = cohere.Client(api_key) 
+response = co.classify(
+  model='5ae71449-3ae0-488f-a703-eb0275839e8f-ft',
+  inputs=[args.paragraph])
+print('The confidence levels of the labels are: {}'.format(response.classifications))
