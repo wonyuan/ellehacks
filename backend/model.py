517792892495af20ab18235bd963b3f14b104aa8
Tvesha Shah <tveshashah13@gmail.com>
2025-02-15 00:59:06 -0500

    models

--- backend/model.py
+++ backend/model.py
@@ -17,9 +17,34 @@ parser.add_argument("paragraph", type=str, help="Input paragraph")
 args = parser.parse_args()
 print(args.paragraph)
 
+situation = args.paragraph
 
 co = cohere.Client(api_key) 
 response = co.classify(
   model='5ae71449-3ae0-488f-a703-eb0275839e8f-ft',
-  inputs=[args.paragraph])
+  inputs=[situation])
 print('The confidence levels of the labels are: {}'.format(response.classifications))
+
+classification = "Angry"
+
+
+if classification == "Angry":
+    chat_id = "ef9183fe-75a5-4686-b7ff-14fced618013-ft"
+elif classification == "Quiet":
+    chat_id = "ef9183fe-75a5-4686-b7ff-14fced618013-ft"
+elif classification == "Judgemental":
+    chat_id = "ef9183fe-75a5-4686-b7ff-14fced618013-ft"
+elif classification == "happy":
+    chat_id = "5340c40f-9e3b-4d16-8d4c-9a1d4495e905-ft"
+
+stream = co.chat_stream( 
+  model=chat_id,
+  message='input scenario here?',
+  temperature=0.3,
+  chat_history=[],
+  prompt_truncation='AUTO'
+) 
+
+for event in stream:
+  if event.event_type == "text-generation":
+    print(event.text, end='')
